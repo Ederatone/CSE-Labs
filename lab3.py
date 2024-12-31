7938cc0150cc6cdeb3bf53a7ba17adbfa2b6dc25
Ederatone <vshkarbanw@ukr.net>
2024-12-31 19:15:15 +0200

    added class to manage task cancellation

--- lab3.py
+++ lab3.py
@@ -1,4 +1,18 @@
 import time
+import concurrent.futures
+import threading
+
+class CancelTask:
+    def __init__(self):
+        self.cancel_event = threading.Event()
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+
+    def cancel(self):
+        self.cancel_event.set()
+
+    def submit(self, task, *args, **kwargs):
+        future = self.executor.submit(task, *args, **kwargs)
+        return future
 
 def long_running_task(cancel_event):
     for i in range(10):
