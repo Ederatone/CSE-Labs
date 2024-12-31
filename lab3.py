c0448936e5119741cf8c876fcf41bbb9ba0c2a61
Ederatone <vshkarbanw@ukr.net>
2024-12-31 19:21:51 +0200

    added demo function

--- lab3.py
+++ lab3.py
@@ -17,8 +17,23 @@ class CancelTask:
 def long_running_task(cancel_event):
     for i in range(10):
         if cancel_event.is_set():
-            print(f"Task has been cancelled.")
+            print(f"Task has been cancelled")
             return
         print(f"Task: Working... ({i+1}/10)")
         time.sleep(1)
-    print(f"Task completed successfully.")
+    print(f"Task completed successfully")
+
+def demo():
+    cancel_task = CancelTask()
+
+    future = cancel_task.submit(long_running_task, cancel_event=cancel_task.cancel_event)
+
+    try:
+        time.sleep(3)
+        print("User decides to cancel the task.")
+        cancel_task.cancel()
+
+        future.result()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
