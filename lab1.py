3c33fdd7f64af07bbd468c79b7beb4a9519cecc7
Ederatone <vshkarbanw@ukr.net>
2024-12-31 14:55:32 +0200

    added demo function

--- lab1.py
+++ lab1.py
@@ -8,3 +8,12 @@ async def async_map(func: Callable, iterable: List[Any]) -> List[Any]:
 async def async_square(number: int) -> int:
     await asyncio.sleep(1)
     return number ** 2
+
+async def demo() -> None:
+    nums = [1, 2, 3, 4, 5, 6]
+
+    print("async_map:")
+    result = await async_map(async_square, nums)
+    print(f"result: {result}")
+
+asyncio.run(demo())
