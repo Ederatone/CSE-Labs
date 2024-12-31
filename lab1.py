49302e48276649ccf82ac3ec3ae7f5c3da9d3065
Ederatone <vshkarbanw@ukr.net>
2024-12-31 14:48:45 +0200

    added async function

--- lab1.py
+++ lab1.py
@@ -4,3 +4,7 @@ from typing import Callable, List, Any
 async def async_map(func: Callable, iterable: List[Any]) -> List[Any]:
     tasks = [func(item) for item in iterable]
     return await asyncio.gather(*tasks)
+
+async def async_square(number: int) -> int:
+    await asyncio.sleep(1)
+    return number ** 2
