1cc1e162e16836bf50ca066c606a59a37f053554
Ederatone <vshkarbanw@ukr.net>
2025-01-03 13:35:06 +0200

    added file processing function

--- lab4.py
+++ lab4.py
@@ -1,6 +1,12 @@
 import asyncio
+import aiofiles
 
 async def process_line(line):
     word_count = len(line.split())
     await asyncio.sleep(0.52)
     print(f"Processed line with {word_count} words.")
+
+async def process_large_file(file_path):
+    async with aiofiles.open(file_path, mode='r') as file:
+        async for line in file:
+            await process_line(line)
