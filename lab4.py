1a55f15aabf6270a8a76b542f50597784e0e089f
Ederatone <vshkarbanw@ukr.net>
2025-01-03 13:37:47 +0200

    added demo function

--- lab4.py
+++ lab4.py
@@ -10,3 +10,7 @@ async def process_large_file(file_path):
     async with aiofiles.open(file_path, mode='r') as file:
         async for line in file:
             await process_line(line)
+
+def demo():
+    file_path = "large_dataset.txt"
+    asyncio.run(process_large_file(file_path))
